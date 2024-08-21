7ea7cbfa118605b6469ba71b826a3e2dc0a62470
apex-creator <82339087+apex-creator@users.noreply.github.com>
2024-08-22 00:34:50 +0530

    Update homepage.js

--- homepage.js
+++ homepage.js
@@ -1,15 +1,4 @@
-      // JavaScript to detect Android and macOS and restrict access
-      function detectDevice() {
-        var userAgent = navigator.userAgent || navigator.vendor || window.opera;
-        var isAndroid = /android/i.test(userAgent);
-        var isMac = /macintosh|mac os x/i.test(userAgent);
-        
-        if (isAndroid || isMac) {
-            document.body.innerHTML = "<h1>Access Restricted</h1><p>This webpage is not accessible on Android and macOS devices.</p>";
-        }
-    }
 
-    window.onload = detectDevice;
 
     document.addEventListener("DOMContentLoaded", function () {
       const frontElements = document.querySelectorAll(".frontstyle");
